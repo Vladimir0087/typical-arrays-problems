455e0deb439278cc54b47111a6962fc06e9243da
Vladimir <17500175@mail.ru>
2020-09-29 09:36:01 +0300

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length===0) return 0;
+    return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length===0) return 0;
+    return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length===0) return 0;
+    let sum = array.reduce((prev, item) => prev + item, 0);
+    let avg = sum / array.length;
+    return avg;
 }
